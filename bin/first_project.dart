51701755b870cfebd777c40a6ba7363dd31edd4c
Nayem Siddiqui <62980753+thesiddiqui@users.noreply.github.com>
2022-09-29 22:26:34 +0600

    Double input output

---
+++ bin/first_project.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+
+ // const String name = 'Nayem';
+ //  int children = 2 + 1;
+ //  double balance = 75.20;
+ //  bool married = true;
+ //
+ //  print(name);
+ //  print(children);
+ //  print(balance);
+ //  print(married);
+
+  // String? x;
+  //
+  // print("enter your name:");
+  // x= stdin.readLineSync();
+  //
+  // print('hi, $x');
+
+ // -----AGE----
+  double? age;
+  print("What's your age?");
+
+  age=double.parse(stdin.readLineSync()!);
+  print("I'm $age years old");
+
+
+// -----NEW AGE----
+
+
+}
